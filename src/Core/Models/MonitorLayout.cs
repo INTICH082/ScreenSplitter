ddd3a958d11b062854cf9b01019704ac6189a973
INTICH082 <official41e41b@gmail.com>
2026-08-20 10:02:27 +0300

    v1.5.2

--- src/Core/Models/MonitorLayout.cs
+++
@@ -1,8 +0,0 @@
-namespace ScreenSplitter.Core.Models;
-
-public class MonitorLayout
-{
-    public string MonitorId { get; init; } = "";
-    public string Name { get; set; } = "Layout 1";
-    public List<Zone> Zones { get; set; } = new();
-}
--- src/Core/Models/Zone.cs
+++
@@ -1,3 +0,0 @@
-namespace ScreenSplitter.Core.Models;
-
-public record Zone(string Id, double X, double Y, double Width, double Height);
